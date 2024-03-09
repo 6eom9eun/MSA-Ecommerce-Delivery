498b0801d0505b60537f8ca3d5cbb64a2741cce9
beomgeun <ggoon98@gmail.com>
2024-03-09 22:56:12 +0900

    ✨ MSA 구축 및 테스트 완료

--- boss/consumer.py
+++ boss/consumer.py
@@ -1,4 +1,6 @@
 import pika 
+import json 
+from main import app, Shop, Order, db
 
 params = pika.URLParameters('amqps://sislvaqt:2-yxbkQ5rB8Q3nMZGif0f5nIMPYKY3bQ@dingo.rmq.cloudamqp.com/sislvaqt')
 
@@ -10,9 +12,43 @@ channel.queue_declare(queue='boss')
 
 def callback(ch, method, properties, body):
     print('Received in boss')
-    print(body)
+    data = json.loads(body)
+    print(data)
 
-channel.basic_consume(queue='boss', on_message_callback=callback)
+    with app.app_context():  # Flask 애플리케이션 컨텍스트 설정
+        if properties.content_type == 'shop_created':
+            shop = Shop(id=data['id'], shop_name=data['shop_name'], shop_address=data['shop_address'])
+            db.session.add(shop)
+            db.session.commit()
+
+        elif properties.content_type == 'shop_updated':
+            shop = Shop.query.get(data['id'])
+            shop.shop_name = data['shop_name']
+            shop.shop_address = data['shop_address']
+            db.session.commit()
+
+        elif properties.content_type == 'shop_deleted':
+            shop = Shop.query.get(data)
+            db.session.delete(shop)
+            db.session.commit()
+
+        elif properties.content_type == 'order_created':
+            order = Order(id=data['id'], shop=data['shop'], address=data['address'])
+            db.session.add(order)
+            db.session.commit()
+
+        elif properties.content_type == 'order_updated':
+            order = Order.query.get(data['id'])
+            order.shop = data['shop']
+            order.address = data['address']
+            db.session.commit()
+
+        elif properties.content_type == 'order_deleted':
+            order = Order.query.get(data)
+            db.session.delete(order)
+            db.session.commit()
+
+channel.basic_consume(queue='boss', on_message_callback=callback, auto_ack=True)
 
 print('Started consuming')
 
--- order/consumer.py
+++ order/consumer.py
@@ -1,4 +1,10 @@
-import pika 
+import pika
+import os, django, json
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",'order.settings')
+django.setup()
+
+from user_order.models import Order
 
 params = pika.URLParameters('amqps://sislvaqt:2-yxbkQ5rB8Q3nMZGif0f5nIMPYKY3bQ@dingo.rmq.cloudamqp.com/sislvaqt')
 
@@ -10,9 +16,13 @@ channel.queue_declare(queue='order')
 
 def callback(ch, method, properties, body):
     print('Received in order')
-    print(body)
+    id = json.loads(body)
+    order = Order.objects.get(id=id)
+    order.deliver_finish = 1
+    order.save()
+    print('order deliver finished')
 
-channel.basic_consume(queue='order', on_message_callback=callback)
+channel.basic_consume(queue='order', on_message_callback=callback, auto_ack=True)
 
 print('Started consuming')
 
